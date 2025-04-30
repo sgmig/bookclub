1fadb7315da1584707be0959bdd3775f46f2f0ac
sgmig <sgmigliaccio@gmail.com>
2025-05-01 00:14:47 +0200

    fix const declaration. Correct function to load details panel

--- clubdelectura/clubs/static/clubs/js/book_rating.js
+++ clubdelectura/clubs/static/clubs/js/book_rating.js
@@ -13,23 +13,23 @@ function loadBookRatingDetails(detailPanelUrl, detailPanelEl) {
 
 document.addEventListener("DOMContentLoaded", function () {
     // Get all the reading list links
-    const clubMeetingLinks = document.querySelectorAll(".club-book-rating-link");
+    const bookRatingLinks = document.querySelectorAll(".book-rating-link");
 
     // Add event listener to each link
-    clubMeetingLinks.forEach(link => {
+    bookRatingLinks.forEach(link => {
         link.addEventListener("click", function (event) {
             event.preventDefault(); // Prevent default link action
 
             // Get the book-rating details url from the clicked link
-            const clubMeetingPartialDetailUrl = link.getAttribute("data-book-rating-partial-details-url")
+            const bookRatingPartialDetailUrl = link.getAttribute("data-book-rating-partial-details-url")
 
             // Show the club book-rating panel
-            meetingDetailsPanelEl = document.getElementById("club-book-rating-details-panel")
-            meetingDetailsPanelEl.style.display = "block";
+            const bookRatingDetailsPanelEl = document.getElementById("book-rating-details-panel")
+            bookRatingDetailsPanelEl.style.display = "block";
 
             // Loading book-rating details template inside the panel
-            console.log(clubMeetingPartialDetailUrl)
-            loadMeetingDetails(clubMeetingPartialDetailUrl, meetingDetailsPanelEl)
+            console.log(bookRatingPartialDetailUrl)
+            loadBookRatingDetails(bookRatingPartialDetailUrl, bookRatingDetailsPanelEl)
 
         });
     });
