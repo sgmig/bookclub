ac80e86d56e2c2e00344801dc66e6f10e0ce2e5f
sgmig <sgmigliaccio@gmail.com>
2026-01-22 17:02:26 +0100

    add location viewset

--- clubdelectura/locations/views.py
+++ clubdelectura/locations/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+# DRF
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from drf_spectacular.utils import extend_schema
+
 # Create your views here.
+
+from locations.models import Location
+from locations.serializers import LocationSerializer
+
+
+@extend_schema(tags=["Locations"])
+class LocationsViewSet(ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [IsAuthenticated]
